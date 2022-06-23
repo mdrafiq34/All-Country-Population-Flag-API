55c4446e10623ea2443c06bffbb989f8e0bff456
Md. Rafiqul Islam <99423768+mdrafiq34@users.noreply.github.com>
2022-06-23 20:24:44 +0600

    Json API

---
+++ script.js
@@ -0,0 +1,24 @@
+function loadPosts() {
+    fetch(`https://restcountries.com/v3.1/all`)
+    .then(res => res.json())
+    .then(posts => displayPosts(posts))
+}
+loadPosts();
+
+
+function displayPosts(posts) {
+    let postsContainer = document.getElementById('post');
+
+    for(let post of posts){
+        let singlePosts = document.createElement('div');
+        singlePosts.classList.add('post');
+        singlePosts.innerHTML = `
+        <h1 class="h" > Country Name: ${post.name.common} </h1>
+        <p class="p"> Population: ${post.population}</p>
+        <p class="p"> Flag: ${post.flags.png}</p>
+        `;
+        postsContainer.appendChild(singlePosts);
+
+    }
+    
+}
